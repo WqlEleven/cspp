767e05460f7f617f3d6d6162ba88ed5936f1759b
WqlEleven <you@example.com2046825227@qq.com>
2019-07-08 13:34:46 +0800

    wql

---
+++ config.js
@@ -0,0 +1,11 @@
+/**
+ * 小程序配置文件
+ */
+
+var config = {
+  host: 'https://pinpin.changshouclub.com',
+  sinaHost: 'http://pinpin-public.stor.sinaapp.com',
+  name: '长寿拼拼'
+};
+
+module.exports = config;
